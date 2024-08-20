c6dd222ba30596cacb5526f56cf97acc5b1f7a5b
Bạch Gia Huy <bghuy2003@gmail.com>
2024-08-20 09:56:30 +0700

    remove useEffect

--- frontend/src/app/quiz/page.js
+++ frontend/src/app/quiz/page.js
@@ -1,10 +1,8 @@
 // pages/quiz/index.js
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import QuizGame from '../../components/QuizGame';
-useEffect(()=>{
-  
-},[])
+
 const QuizPage = () => (
   <div>
     <h1>Quiz Game</h1>
