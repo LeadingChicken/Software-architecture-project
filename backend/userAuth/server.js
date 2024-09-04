f78741e6ccc085aabc3c4317172d3616a9c5e857
Giant09dev <aidimi2810.sgn@gmail.com>
2024-09-04 10:33:06 +0700

    cap nhat backend

--- backend/userAuth/server.js
+++ backend/userAuth/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const userApi = require('./api/userApi');
+const express = require("express");
+const connectDB = require("./config/db");
+const userApi = require("./api/userApi");
+const cors = require("cors");
 
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
-app.use('/api', userApi);
+app.use("/api", userApi);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`);
+  console.log(`Server is running on port ${PORT}!`);
 });
