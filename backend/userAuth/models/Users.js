00716eed17fb56c6c379bf62375333cead94a2f3
Giant09dev <aidimi2810.sgn@gmail.com>
2024-09-04 11:11:59 +0700

    update model Users
    add fields: email, fullName, phoneNumber

--- backend/userAuth/models/Users.js
+++ backend/userAuth/models/Users.js
@@ -1,19 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    isAdmin: {
-        type: Boolean
-    }
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+  },
+  email: {
+    type: String,
+    //required: true,
+  },
+  fullName: {
+    type: String,
+    //required: true,
+  },
+  phoneNumber: {
+    type: String,
+    //required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User; 
+const User = mongoose.model("User", userSchema);
+module.exports = User;
